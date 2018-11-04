2c2ad516f928801c2273027bd4a9472635736e6b
Wandalen <wandalen.me@gmail.com>
2018-11-04 21:11:16 +0200

    auto

---
+++ proto/dwtools/atop/tester/Include.mid.s
@@ -0,0 +1,16 @@
+(function _Include_mid_s_() {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  require( './Include.base.s' );
+
+  require( './l5/Routine.s' );
+  require( './l5/Suite.s' );
+  require( './l5/Tester.s' );
+
+}
+
+})();

e6fc29caaac789256ca7f8210cdd80e8b60b6f9f
Wandalen <wandalen.me@gmail.com>
2018-11-22 16:42:47 +0200

    auto

--- out.debug/dwtools/atop/tester/l5/Tester.s
+++ out.debug/dwtools/atop/tester/l5/Tester.s
@@ -252,7 +252,7 @@ function _includeTestsFrom( path )
   let files = _.fileProvider.filesFind
   ({
     filePath : path,
-    recursive : 1,
+    recursive : '2',
     filter :
     {
       ends : ends,

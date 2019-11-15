3c616c47cc021caeec9aa92e6b496ebc669acda6
vmigdal <volodymyr.migdal@gmail.com>
2019-11-15 11:10:45 +0200

    spectron/Element sample

---
+++ sample/spectron/Element.test.s
@@ -0,0 +1,103 @@
+( function _Element_test_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  let _ = require( 'wTools' );
+  _.include( 'wTesting' );
+  _.include( 'wFiles' );
+
+  var ElectronPath = require( 'electron' );
+  var Spectron = require( 'spectron' );
+
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+// --
+// context
+// --
+
+function onSuiteBegin()
+{
+  let self = this;
+  self.tempDir = _.path.pathDirTempOpen( _.path.join( __dirname, '../..'  ), 'Tester' );
+  self.assetDirPath = _.path.join( __dirname, 'asset' );
+}
+
+function onSuiteEnd()
+{
+  let self = this;
+  _.assert( _.strHas( self.tempDir, 'Tester' ) )
+  _.path.pathDirTempClose( self.tempDir );
+}
+
+// --
+// tests
+// --
+
+//
+
+async function element( test )
+{
+  let self = this;
+  let routinePath = _.path.join( self.tempDir, test.name );
+  let mainPath = _.path.nativize( _.path.join( routinePath, 'main.js' ) );
+
+  _.fileProvider.filesReflect({ reflectMap : { [ self.assetDirPath ] : routinePath } })
+
+  let app = new Spectron.Application
+  ({
+    path : ElectronPath,
+    args : [ mainPath ]
+  })
+
+  await app.start()
+  await app.client.waitUntilTextExists( 'p','Hello world', 5000 )
+
+  test.case = 'Check element html'
+  var got = await app.client.$( '.class1 p' ).getHTML();
+  test.identical( got, '<p>Text1</p>' )
+
+  await app.stop();
+
+  return null;
+}
+
+// --
+// suite
+// --
+
+var Self =
+{
+
+  name : 'Visual.Spectron.Element',
+  silencing : 1,
+  enabled : 1,
+
+  onSuiteBegin : onSuiteBegin,
+  onSuiteEnd : onSuiteEnd,
+  routineTimeOut : 300000,
+
+  context :
+  {
+    tempDir : null,
+    assetDirPath : null,
+  },
+
+  tests :
+  {
+    element,
+  }
+
+}
+
+//
+
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();

af2fcc6b5f2ab6dc420c8cbf339a3c8d3a8ab613
Volodymyr Migdal <volodymyr.migdal@gmail.com>
2019-11-20 14:51:29 +0200

    puppeteer inject script sample

---
+++ sample/puppeteer/InjectScript.test.s
@@ -0,0 +1,102 @@
+( function _InjectScript_test_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  let _ = require( 'wTools' );
+  _.include( 'wTesting' );
+  _.include( 'wFiles' );
+
+  var Puppeteer = require( 'puppeteer' );
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+// --
+// context
+// --
+
+function onSuiteBegin()
+{
+  let self = this;
+
+  self.tempDir = _.path.pathDirTempOpen( _.path.join( __dirname, '../..'  ), 'Tester' );
+  self.assetDirPath = _.path.join( __dirname, 'asset' );
+}
+
+//
+
+function onSuiteEnd()
+{
+  let self = this;
+  _.assert( _.strHas( self.tempDir, 'Tester' ) )
+  _.path.pathDirTempClose( self.tempDir );
+}
+
+// --
+// tests
+// --
+
+async function injectScript( test )
+{
+  let self = this;
+  let routinePath = _.path.join( self.tempDir, test.name );
+  let indexHtmlPath = _.path.join( routinePath, 'index.html' );
+
+  _.fileProvider.filesReflect({ reflectMap : { [ self.assetDirPath ] : routinePath } })
+
+  let browser = await Puppeteer.launch({ headless : true });
+  let page = await browser.newPage();
+
+  await page.goto( 'file:///' + _.path.nativize( indexHtmlPath ), { waitUntil : 'load' } );
+
+  let got = await page.evaluate( () => 
+  {
+    let element = document.querySelector( 'p' );
+    element.innerText = 'Hello from test';
+    return element.innerText;
+  })
+  test.identical( got, 'Hello from test' );
+
+  await browser.close();
+
+  return null;
+}
+
+// --
+// suite
+// --
+
+var Self =
+{
+
+  name : 'Visual.Puppeteer.Input',
+  
+  
+
+  onSuiteBegin : onSuiteBegin,
+  onSuiteEnd : onSuiteEnd,
+  routineTimeOut : 300000,
+
+  context :
+  {
+    tempDir : null,
+    assetDirPath : null,
+  },
+
+  tests :
+  {
+    injectScript
+  }
+
+}
+
+//
+
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();

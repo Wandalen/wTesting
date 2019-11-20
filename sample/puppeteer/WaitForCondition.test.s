f3ee96d9a9350423974b0aaf8a89e333f8542af1
Volodymyr Migdal <volodymyr.migdal@gmail.com>
2019-11-20 15:21:21 +0200

    puppeteer wait for condition tutorial

---
+++ sample/puppeteer/WaitForCondition.test.s
@@ -0,0 +1,113 @@
+( function _WaitForCondition_test_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  let _ = require( 'wTools' );
+  _.include( 'wTesting' );
+  _.include( 'wFiles' );
+
+  var Puppeteer = require( 'puppeteer' );
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+// --
+// context
+// --
+
+function onSuiteBegin()
+{
+  let self = this;
+
+  self.tempDir = _.path.pathDirTempOpen( _.path.join( __dirname, '../..'  ), 'Tester' );
+  self.assetDirPath = _.path.join( __dirname, 'asset' );
+}
+
+//
+
+function onSuiteEnd()
+{
+  let self = this;
+  _.assert( _.strHas( self.tempDir, 'Tester' ) )
+  _.path.pathDirTempClose( self.tempDir );
+}
+
+// --
+// tests
+// --
+
+async function waitForTextChange( test )
+{
+  let self = this;
+  let routinePath = _.path.join( self.tempDir, test.name );
+  let indexHtmlPath = _.path.join( routinePath, 'index.html' );
+
+  _.fileProvider.filesReflect({ reflectMap : { [ self.assetDirPath ] : routinePath } })
+
+  let browser = await Puppeteer.launch({ headless : true });
+  let page = await browser.newPage();
+
+  await page.goto( 'file:///' + _.path.nativize( indexHtmlPath ), { waitUntil : 'load' } );
+
+  //Change text property after delay
+  _.timeOut( 1500, () => 
+  {
+    page.$eval( 'p', ( e ) => 
+    {  
+      e.innerText = 'Hello from test'
+    })
+  })
+
+  //Wait until text will be changed
+  await page.waitFor( () => 
+  {
+    return document.querySelector( 'p' ).innerText === 'Hello from test';
+  })
+
+  //Check text property
+  var got = await page.$eval( 'p', ( e ) => e.innerText )
+  test.identical( got, 'Hello from test' );
+
+  await browser.close();
+
+  return null;
+}
+
+// --
+// suite
+// --
+
+var Self =
+{
+
+  name : 'Visual.Puppeteer.Input',
+  
+  
+
+  onSuiteBegin : onSuiteBegin,
+  onSuiteEnd : onSuiteEnd,
+  routineTimeOut : 300000,
+
+  context :
+  {
+    tempDir : null,
+    assetDirPath : null,
+  },
+
+  tests :
+  {
+    waitForTextChange
+  }
+
+}
+
+//
+
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();

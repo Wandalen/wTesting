1c3afbc38eb2e33aaa0eaf426e3850e5606c0a35
Wandalen <wandalen.me@gmail.com>
2020-02-05 11:05:47 +0200

    add missing test assets

---
+++ proto/dwtools/atop/tester.test/_asset/checksAfterTimeOutSilenced/Suite.test.js
@@ -0,0 +1,65 @@
+
+require( 'wTesting' );
+let _ = require( 'wTools' );
+_.include( 'wConsequence' );
+
+//
+
+function routine1( test )
+{
+  test.description = 'description1';
+  console.log( 'v0' );
+  test.identical( 1, 1 );
+  test.description = 'description2';
+
+  _.time.out( 1000, () =>
+  {
+    console.log( 'v1' );
+    test.identical( 1, 1 );
+    test.equivalent( 1, 1 );
+    console.log( 'v2' );
+  });
+  _.time.out( 2000, () =>
+  {
+    console.log( 'v3' );
+    test.identical( 1, 1 );
+    test.equivalent( 1, 1 );
+    console.log( 'v4' );
+  });
+
+}
+
+routine1.timeOut = 100;
+
+//
+
+function routine2( test )
+{
+  test.description = 'description1';
+  console.log( 'v10' );
+  test.identical( 1, 1 );
+  return _.time.out( 2000, () =>
+  {
+  })
+}
+
+routine2.timeOut = 10000;
+
+//
+
+var Self1 =
+{
+  name : 'ChecksAfterTimeOutSilencedAsset',
+  silencing : 1,
+  tests :
+  {
+    routine1,
+    routine2,
+  }
+}
+
+//
+
+Self1 = wTestSuite( Self1 );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self1.name );
---
+++ proto/dwtools/atop/tester.test/_asset/timeOutExternalMessage/Suite.test.js
@@ -0,0 +1,63 @@
+
+require( 'wTesting' );
+
+let _ = _testerGlobal_.wTools;
+
+_.include( 'wProcedure' );
+_.include( 'wConsequence' );
+
+//
+
+function procedure1( test )
+{
+
+  test.case = 'give msg before timeOut';
+  var t = 100;
+
+  var con1 = _.time.out( t*2, () => 1 );
+
+  console.log( 'v1' )
+  _.time.out( t, function()
+  {
+    console.log( 'v2' )
+    con1.take( 2 );
+    con1.give( ( err, got ) =>
+    {
+      console.log( 'v3' )
+    })
+    con1.give( ( err, got ) =>
+    {
+      console.log( 'v4' )
+    })
+    con1.give( ( err, got ) =>
+    {
+      console.log( 'v5' )
+    })
+  })
+
+  return _.time.out( t*5 ).then( () =>
+  {
+    test.identical( con1.argumentsCount(), 0 );
+    test.identical( con1.errorsCount(), 0 );
+    test.identical( con1.competitorsCount(), 1 );
+    con1.cancel();
+    return null;
+  });
+}
+
+//
+
+var Self1 =
+{
+  name : 'Bug1TestSuite',
+  tests :
+  {
+    procedure1,
+  }
+}
+
+//
+
+Self1 = wTestSuite( Self1 );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self1.name );

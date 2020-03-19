410abdbe90d7d0e280bc102221352c86e9a7d2d8
Dmytro <dm.vict.kr@gmail.com>
2020-03-19 09:33:53 +0200

    Add sample `testRoutineStepByStep`

---
+++ sample/testRoutineStepByStep/Number.js
@@ -0,0 +1,10 @@
+let _ = require( 'wTools' );
+
+function numberIs( src )
+{
+  _.assert( arguments.length === 1, 'Expects single argument {-src-}' );
+
+  return typeof src === 'number';
+}
+
+module.exports.numberIs = numberIs;

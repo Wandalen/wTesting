b1a642cf05ad9c466046feedc278ed4087e5d21c
wandalen <wandalen.me@gmail.com>
2020-12-21 12:38:13 +0200

    version 1.0.432

--- proto/wtools/atop/testing.test/_asset/asyncTimeOut/Suite.test.js
+++ proto/wtools/atop/testing.test/_asset/asyncTimeOut/Suite.test.js
@@ -5,12 +5,25 @@ _.include( 'wConsequence' );
 
 //
 
+// function routine1( test )
+// {
+//   test.description = 'description1';
+//   console.log( 'routine1.1' );
+//   _.time.out( 2000, () => console.log( '_.time.out( 2000 )' ) );
+//   return _.time.out( 1000, () =>
+//   {
+//     console.log( 'routine1.2' );
+//     test.identical( 1, 1 );
+//     console.log( 'routine1.3' );
+//   })
+// }
+
 function routine1( test )
 {
+  let context = this;
   test.description = 'description1';
   console.log( 'routine1.1' );
-  _.time.out( 2000, () => console.log( '_.time.out( 2000 )' ) );
-  return _.time.out( 1000, () =>
+  return _.time.out( context.t2 * 2, () =>
   {
     console.log( 'routine1.2' );
     test.identical( 1, 1 );
@@ -18,7 +31,7 @@ function routine1( test )
   })
 }
 
-routine1.timeOut = 500;
+routine1.timeOut = 5000;
 
 //
 
@@ -26,6 +39,10 @@ var Self1 =
 {
   name : 'AsyncTimeOutAsset',
   silencing : 0,
+  context : 
+  {
+    t2 : 5000,
+  },
   tests :
   {
     routine1,

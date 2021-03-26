245ebdc26d3e0dd086d090a9f16a840e3e0795bd
wandalen <wandalen.me@gmail.com>
2021-03-27 01:23:01 +0200

    .

--- proto/wtools/abase/l0/l0/Global.s
+++ proto/wtools/abase/l0/l0/Global.s
@@ -28,10 +28,8 @@ if( !_global._globals_ )
 
 //
 
-//globalNamespaceOpen
 function _new( name, global )
 {
-  // const Module = require( 'module' );
 
   if( _realGlobal_._globals_[ name ] )
   throw Error( `Global namespace::${name} already exists!` );
@@ -45,20 +43,12 @@ function _new( name, global )
   global2.wTools.module.nativeFilesMap = Object.create( null );
 
   return global2;
-  // return _realGlobal_.wTools.global.open( name );
 }
 
 //
 
 function open( name )
 {
-  // const Module = require( 'module' );
-
-  // _global_.wTools = Object.hasOwnProperty.call( _global_, 'wTools' ) ? _global_.wTools : Object.create( null );
-  // _global_.wTools.global = _global_.wTools.global || Object.create( null );
-  // _global_.wTools.global.current = _global_.wTools.global.current || _global_;
-  // _global_.wTools.module = _global_.wTools.module || Object.create( null );
-  // _global_.wTools.module.nativeFilesMap = _global_.wTools.module.nativeFilesMap || Module._cache;
 
   if( !_global_.wTools.global.current )
   throw Error( 'Global namespace is not setup' );
@@ -69,39 +59,37 @@ function open( name )
   throw Error( `Global namespace::${name} deos not exist!` );
   if( !_global_.__GLOBAL_NAME__ )
   throw Error( `Current global namespace deos not have name!` );
-  if( _global_.wTools.module.nativeFilesMap !== Module._cache )
+  if( _global_.wTools.module.nativeFilesMap !== ModuleFileNative._cache )
   throw Error( `Current global have native module files map of different global` );
 
   _realGlobal_.wTools.global._stack.push
   ({
     name : _global_.__GLOBAL_NAME__,
     global : _global_,
-    moduleNativeFilesMap : Module._cache,
+    moduleNativeFilesMap : ModuleFileNative._cache,
   });
 
   let global2 = _globals_[ name ];
   _realGlobal_._global_ = global2;
-  Module._cache = global2.wTools.module.nativeFilesMap;
+  ModuleFileNative._cache = global2.wTools.module.nativeFilesMap;
 
   return global2;
 }
 
 //
 
-// globalNamespaceClose
 function close( name )
 {
-  // const Module = require( 'module' );
 
   if( name !== _global_.__GLOBAL_NAME__ )
   throw Error( `Current global is ${_global_.__GLOBAL_NAME__}, not ${name}` );
-  if( _global_.wTools.module.nativeFilesMap !== Module._cache )
+  if( _global_.wTools.module.nativeFilesMap !== ModuleFileNative._cache )
   throw Error( `Current global have native module files map of different global` );
   if( !_realGlobal_.wTools.global._stack.length )
   throw Error( `Nothing to close` );
 
   let was = _realGlobal_.wTools.global._stack.pop();
-  Module._cache = was.moduleNativeFilesMap;
+  ModuleFileNative._cache = was.moduleNativeFilesMap;
   _realGlobal_._global_ = was.global;
 }
 
@@ -176,9 +164,9 @@ function fileNativeIs( src )
 {
   if( !src )
   return false;
-  if( !Module )
+  if( !ModuleFileNative )
   return true;
-  return src instanceof Module;
+  return src instanceof ModuleFileNative;
 }
 
 //
@@ -206,12 +194,10 @@ function fileNativeFrom( src )
 
 //
 
-const Module = typeof module !== 'undefined' ? require( 'module' ) : null;
+const ModuleFileNative = typeof module !== 'undefined' ? require( 'module' ) : null;
 const __ = _realGlobal_.wTools = _realGlobal_.wTools || Object.create( null );
 __.global = __.global || Object.create( null );
-// __.global.current = __.global.current || _realGlobal_;
 __.module = __.module || Object.create( null );
-// __.module.nativeFilesMap = __.module.nativeFilesMap || ( typeof module !== 'undefined' ? require( 'module' )._cache : null );
 
 __.global._stack = __.global._stack || [];
 __.global.new = __.global.new || _new;

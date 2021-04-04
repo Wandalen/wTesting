f1bf6cddb76b7c39580826e03df7c8bbf4f58859
wandalen <wandalen.me@gmail.com>
2021-04-04 07:17:49 +0300

    .

--- proto/wtools/atop/testing/l5/Routine.s
+++ proto/wtools/atop/testing/l5/Routine.s
@@ -665,9 +665,8 @@ function _runnableGet()
 
   if( suite.routine )
   {
-
     if( _.arrayIs( suite.routine ) )
-    return _.any( suite.routine, ( e ) => _.path.globShortFit( tro.name, e ) )
+    return !!_.any( suite.routine, ( e ) => _.path.globShortFit( tro.name, e ) )
     return _.path.globShortFit( tro.name, suite.routine );
   }
 

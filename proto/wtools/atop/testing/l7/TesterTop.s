4d8c4a54eb97ce0cac85ce3ba8a7d483712c09c0
dmvict <dm.vict.kr@gmail.com>
2021-05-27 10:03:58 +0300

    Improve private routine `_commandHandleSyntaxError`, use routine `strRequestParse` instead of `strCommandParse`

--- proto/wtools/atop/testing/l7/TesterTop.s
+++ proto/wtools/atop/testing/l7/TesterTop.s
@@ -101,9 +101,8 @@ function _commandHandleSyntaxError( o )
 {
   let cui = this;
   let aggregator = cui.aggregator;
-  let request = _.strCommandParse({ src : o.command, commandFormat : 'subject? options?' });
-  // let request = _.strCommandParse({ src : o.command, commandFormat : 'subject options?' }); /* Dmytro : first 2 arguments of input arguments are missed in routine input, so o.command can be an options map with this syntax r:"[ one, two ]" */
-  // let request = _.strRequestParse({ src : o.command }); /* Dmytro : alternative variant */
+  // let request = _.strCommandParse({ src : o.command, commandFormat : 'subject? options?' });
+  let request = _.strRequestParse({ src : o.command });
   return aggregator.instructionPerform({ command : '.run ' + request.subject, propertiesMap : request.map });
 }
 

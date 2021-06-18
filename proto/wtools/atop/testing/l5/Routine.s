0e7925d49c3132fa2fad65c9cef858eeb541e9f6
dmvict <dm.vict.kr@gmail.com>
2021-06-18 09:12:59 +0300

    Extend private routine `_runnableGet`, return `false` for selector with globs if routine is experimental

--- proto/wtools/atop/testing/l5/Routine.s
+++ proto/wtools/atop/testing/l5/Routine.s
@@ -666,9 +666,19 @@ function _runnableGet()
 
   if( suite.routine )
   {
-    if( _.arrayIs( suite.routine ) )
-    return !!_.any( suite.routine, ( e ) => _.path.globShortFit( tro.name, e ) )
-    return _.path.globShortFit( tro.name, suite.routine );
+    // if( _.arrayIs( suite.routine ) )
+    // return !!_.any( suite.routine, ( e ) => _.path.globShortFit( tro.name, e ) )
+    // return _.path.globShortFit( tro.name, suite.routine );
+
+    let result = false;
+    const checkRoutine = tro.experimental ? checkExperimental : checkNotExperimental;
+    if( _.long.is( suite.routine ) )
+    for( let i = 0 ; i < suite.routine.length ; i++ )
+    result = checkRoutine( tro.name, suite.routine[ i ] );
+    else
+    result = checkRoutine( tro.name, suite.routine );
+
+    return result;
   }
 
   if( tro.experimental )
@@ -678,6 +688,22 @@ function _runnableGet()
   return false;
 
   return true;
+
+  /* */
+
+  function checkExperimental( name, selector )
+  {
+    if( !_.path.isGlob( selector ) )
+    return _.path.globShortFit( name, selector );
+    return false;
+  }
+
+  /* */
+
+  function checkNotExperimental( name, selector )
+  {
+    return _.path.globShortFit( name, selector );
+  }
 }
 
 //

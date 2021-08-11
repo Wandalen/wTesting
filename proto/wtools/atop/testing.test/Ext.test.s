a83b51c355d7f2ae3349fed9b21599fb49b40b25
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-08-11 10:04:12 +0300

    test for typescript support feature

--- proto/wtools/atop/testing.test/Ext.test.s
+++ proto/wtools/atop/testing.test/Ext.test.s
@@ -7090,6 +7090,47 @@ checkDiffWithProtoContainsOnly.description =
 Check diff from test.containsOnly, when comparing maps that set new __proto__.
 `
 
+//
+
+function typescriptSupport( test )
+{
+  let context = this;
+  let a = context.assetFor( test, 'typescriptSupport' );
+
+  a.reflect();
+
+  /* */
+
+  a.ready.then( () =>
+  {
+    test.case = 'detects and runs typescript tests';
+    return null;
+  })
+
+  a.appStartNonThrowing({ args : [ '.run', a.abs( 'tests/**' ) ] })
+
+  .then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    
+    test.identical( _.strCount( op.output, 'Passed TestSuite::TestSuite1.test.ts' ), 1 );
+    test.identical( _.strCount( op.output, 'Passed TestSuite::TestSuite2.test.ts' ), 1 );
+    test.identical( _.strCount( op.output, 'Passed test routines 2 / 2' ), 1 );
+    test.identical( _.strCount( op.output, 'Passed test suites 2 / 2' ), 1 );
+
+    return null;
+  })
+
+  /* */
+
+  return a.ready;
+}
+
+typescriptSupport.description = 
+`
+  - Tester should detect typescript test files and include them into the run.
+`
+
 // --
 // suite
 // --
@@ -7200,7 +7241,11 @@ const Proto =
     checkDiffWithProtoContains,
     checkDiffWithProtoContainsAll,
     checkDiffWithProtoContainsAny,
-    checkDiffWithProtoContainsOnly
+    checkDiffWithProtoContainsOnly,
+
+    // typescript
+
+    typescriptSupport,
 
   }
 
